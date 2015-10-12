44ac5a74c0de6644acfbae80f05f73c8c57d9f4c
Sajad KK <kksajad@gmail.com>
2015-10-12 22:07:34 +0530

    montary widget for list view

---
+++ listview_monetary/__openerp__.py
@@ -0,0 +1,41 @@
+##############################################################################
+#
+#    Sajad KK <kksajad@gmail.com>
+#    Copyright (C) 2015
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    'name': 'Monetary widget for list view',
+    'version': '1.0.0',
+    'sequence': 150,
+    'category': 'Web',
+    'summary': 'Monetary widget for list view',
+    'description': """
+This module implements a monetary widget for tree view. The model should have a currency field 'currency_id'.
+    eg. usage: 
+             <field name="currency_id" invisible="1" />
+
+             <field name="amount_total" widget="monetary" />
+
+""",
+    'author': 'Sajad KK <kksajad@gmail.com>',
+    'depends': ['web'],
+    'data': [],
+    'js': ['static/src/js/view_list.js'],	
+    'installable': True,
+    'application': False,
+    'auto_install': False
+}
---
+++ listview_monetary/static/src/js/view_list.js
@@ -0,0 +1,147 @@
+openerp.listview_monetary = function(instance) {
+    instance.web.list.columns.add('field.monetary','instance.web.list.FieldMonetary');
+
+    instance.web.list.FieldMonetary = instance.web.list.Column.extend({
+        /**
+         * Return amount with currency symbol
+         *
+         * @private
+         */
+        init: function() {
+            this._super.apply(this, arguments);
+            this.cur_map = {
+                "ALL": "L"
+                , "AED": "د.إ"
+                , "AFN": "؋"
+                , "ARS": "$"
+                , "AWG": "ƒ"
+                , "AUD": "$"
+                , "AZN": "m"
+                , "BSD": "$"
+                , "BBD": "$"
+                , "BYR": "p."
+                , "BZD": "BZ$"
+                , "BMD": "$"
+                , "BOB": "Bs."
+                , "BAM": "KM"
+                , "BWP": "P"
+                , "BGN": "лв"
+                , "BRL": "R$"
+                , "BND": "$"
+                , "KHR": "៛"
+                , "CAD": "$"
+                , "KYD": "$"
+                , "CLP": "$"
+                , "CNY": "¥"
+                , "COP": "$"
+                , "CRC": "₡"
+                , "HRK": "kn"
+                , "CUP": "₱"
+                , "CZK": "Kč"
+                , "DKK": "kr"
+                , "DOP": "RD$"
+                , "XCD": "$"
+                , "EGP": "£"
+                , "SVC": "$"
+                , "EEK": "kr"
+                , "EUR": "€"
+                , "FKP": "£"
+                , "FJD": "$"
+                , "GHC": "¢"
+                , "GIP": "£"
+                , "GTQ": "Q"
+                , "GGP": "£"
+                , "GYD": "$"
+                , "HNL": "L"
+                , "HKD": "$"
+                , "HUF": "Ft"
+                , "ISK": "kr"
+                , "INR": "₹"
+                , "IDR": "Rp"
+                , "IRR": "﷼"
+                , "IMP": "£"
+                , "ILS": "₪"
+                , "JMD": "J$"
+                , "JPY": "¥"
+                , "JEP": "£"
+                , "KES": "KSh"
+                , "KZT": "лв"
+                , "KPW": "₩"
+                , "KRW": "₩"
+                , "KGS": "лв"
+                , "LAK": "₭"
+                , "LVL": "Ls"
+                , "LBP": "£"
+                , "LRD": "$"
+                , "LTL": "Lt"
+                , "MKD": "ден"
+                , "MYR": "RM"
+                , "MUR": "₨"
+                , "MXN": "$"
+                , "MNT": "₮"
+                , "MZN": "MT"
+                , "NAD": "$"
+                , "NPR": "₨"
+                , "ANG": "ƒ"
+                , "NZD": "$"
+                , "NIO": "C$"
+                , "NGN": "₦"
+                , "NOK": "kr"
+                , "OMR": "﷼"
+                , "PKR": "₨"
+                , "PAB": "B/."
+                , "PYG": "Gs"
+                , "PEN": "S/."
+                , "PHP": "₱"
+                , "PLN": "zł"
+                , "QAR": "﷼"
+                , "RON": "lei"
+                , "RUB": "₽"
+                , "SHP": "£"
+                , "SAR": "﷼"
+                , "RSD": "Дин."
+                , "SCR": "₨"
+                , "SGD": "$"
+                , "SBD": "$"
+                , "SOS": "S"
+                , "ZAR": "R"
+                , "LKR": "₨"
+                , "SEK": "kr"
+                , "CHF": "Fr."
+                , "SRD": "$"
+                , "SYP": "£"
+                , "TZS": "TSh"
+                , "TWD": "NT$"
+                , "THB": "฿"
+                , "TTD": "TT$"
+                , "TRY": ""
+                , "TRL": "₤"
+                , "TVD": "$"
+                , "UGX": "USh"
+                , "UAH": "₴"
+                , "GBP": "£"
+                , "USD": "$"
+                , "UYU": "$U"
+                , "UZS": "лв"
+                , "VEF": "Bs"
+                , "VND": "₫"
+                , "YER": "﷼"
+                , "ZWD": "Z$"
+                };
+        },
+        _format: function (row_data, options) {
+            var self = this;
+            if (this.type == "float") {
+                if (row_data.currency_id) {
+                    var symbol = "";
+                    if(self.cur_map[row_data.currency_id.value[1]]!==undefined) {
+                        symbol = self.cur_map[row_data.currency_id.value[1]]
+                    }
+                    var amount = _.escape(instance.web.format_value(row_data[this.id].value, {'type': 'float'}));
+                    return _.template('<span><%-symbol%><%-amount%></span>', {'amount': amount, 'symbol': symbol});
+                }
+                return _.escape(instance.web.format_value(row_data[this.id].value, {'type': 'float'}, options.value_if_empty));
+            }
+        }
+    });
+};
